89a44753aa318afc0678cdf704461dbbe3a58773
EC2 Default User <ec2-user@ip-172-31-61-20.ec2.internal>
2022-01-31 19:48:34 +0000

    lab2

---
+++ lec2.py
@@ -0,0 +1,20 @@
+"""
+lec 2 
+this is a regional comment
+""" 
+
+#print ("hello world") # this is a single line comment
+#print (123)
+"""
+print (123.)
+print("123")
+print(type (123))
+print (type(123.))
+print (type("123"))
+print("It's our 2nd Python class")
+"""
+print ("Hello world".upper())
+print ("Hello world".split())
+print ("hello" + "world")
+#two asterisks for exponents ex 2**3 = 8
+print (2**1/3)
