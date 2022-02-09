50a1d59eb34659ed22b056f61b197794ca2040e7
EC2 Default User <ec2-user@ip-172-31-61-20.ec2.internal>
2022-02-09 19:54:15 +0000

    added my lab3.py file.

---
+++ lec3.py
@@ -0,0 +1,23 @@
+'''
+lec 3
+'''
+
+my_list = [1, 2, 3, 4, 5 ]
+print(my_list)
+
+my_nested_list = [1, 2, 3, [1, 2, 3, 4, 5 ]]
+
+print(my_nested_list)
+
+my_list[0]=6
+
+print(my_list[0])
+print(my_list[1])
+print(my_list[-1])
+
+print(my_list[1:3])
+
+my_list.append(7)
+print(my_list)
+my_list.remove(7)
+
